5c097b3ddb35dfe93f9bf08fca5e21369189bf02
Ilya Ivanov <static.ila@gmail.com>
2021-07-01 17:09:35 +0300

    Finish vertical movement

--- domain/actions.spec.ts
+++ domain/actions.spec.ts
@@ -13,6 +13,31 @@ it("having two items with first selected going down selects second item", () => 
   expect(actions.onDownArrow(state).nextState.selectedItem).toBe("2");
 });
 
+it("having two items with first selected going down selects second item", () => {
+  const state: AppState = {
+    items: buildItems(`
+        HOME
+          1
+          2
+      `),
+    selectedItem: "2",
+  };
+  expect(actions.onDownArrow(state).nextState.selectedItem).toBe("2");
+});
+
+it("having two items with first selected going down selects second item", () => {
+  const state: AppState = {
+    items: buildItems(`
+        HOME
+          1
+          2
+            2.1
+      `),
+    selectedItem: "2.1",
+  };
+  expect(actions.onDownArrow(state).nextState.selectedItem).toBe("2.1");
+});
+
 it("having two items with second selected going up selects first item", () => {
   const state: AppState = {
     items: buildItems(`
@@ -23,4 +48,58 @@ it("having two items with second selected going up selects first item", () => {
     selectedItem: "2",
   };
   expect(actions.onUpArrow(state).nextState.selectedItem).toBe("1");
+});
+
+it("going down when selected item is open and has children selects first child", () => {
+  const state: AppState = {
+    items: buildItems(`
+        HOME
+          1 -open
+            1.1
+          2
+      `),
+    selectedItem: "1",
+  };
+  expect(actions.onDownArrow(state).nextState.selectedItem).toBe("1.1");
+});
+
+it("going up selects parent if selected item is first in context", () => {
+  const state: AppState = {
+    items: buildItems(`
+        HOME
+          1 -open
+            1.1
+          2
+      `),
+    selectedItem: "1.1",
+  };
+  expect(actions.onUpArrow(state).nextState.selectedItem).toBe("1");
+});
+
+it("going down when selected item is the last in context selects next item", () => {
+  const state: AppState = {
+    items: buildItems(`
+        HOME
+          1 -open
+            1.1 -open
+              1.1.1
+          2
+      `),
+    selectedItem: "1.1.1",
+  };
+  expect(actions.onDownArrow(state).nextState.selectedItem).toBe("2");
+});
+
+it("going up when selects the most nested items if previous item is open", () => {
+  const state: AppState = {
+    items: buildItems(`
+        HOME
+          1 -open
+            1.1 -open
+              1.1.1
+          2
+      `),
+    selectedItem: "2",
+  };
+  expect(actions.onUpArrow(state).nextState.selectedItem).toBe("1.1.1");
 });
--- domain/items.ts
+++ domain/items.ts
@@ -70,34 +70,39 @@ export const setChildren = (items: Items, id: string, children: Item[]) => {
   items[id].children = children.map((item) => item.id);
 };
 
-export const getNextBelow = (items: Items, id: string): string | undefined => {
-  const children = items[id].children;
-  if (isOpen(items, id) && children && children.length > 0) return children[0];
-  else {
-    const context = getContext(items, id);
-    const index = context.indexOf(id);
-    if (index < context.length - 1) return context[index + 1];
+export function getNextBelow(items: Items, itemId: string): string | undefined {
+  const children = items[itemId].children;
+  if (isOpen(items, itemId) && children && children.length > 0) {
+    return children[0];
+  } else {
+    const followingItem = getFollowingItem(items, itemId);
+    if (followingItem) {
+      return followingItem;
+    } else {
+      let parentId = getParentId(items, itemId); //?
+      while (!isRoot(parentId) && parentId && isLast(items, parentId)) {
+        parentId = getParentId(items, parentId);
+      }
+      if (parentId && !isRoot(parentId)) {
+        const following = getFollowingItem(items, parentId);
+        if (following) return following;
+      }
+    }
   }
-};
+}
 
 export const getItemAbove = (items: Items, id: string): string | undefined => {
   const context = getContext(items, id);
   const index = context.indexOf(id);
-  if (index > 0) return context[index - 1];
-};
-
-const getParentId = (items: Items, id: string): string => {
-  const i = Object.keys(items).find((key) => {
-    const item = items[key];
-    return item.children && item.children.indexOf(id) >= 0;
-  });
-
-  return i!;
-};
-
-const getContext = (items: Items, id: string): string[] => {
-  const parent = getParentId(items, id);
-  return items[parent].children || [];
+  if (index > 0) {
+    const prevItemId = context[index - 1];
+    return isOpen(items, prevItemId)
+      ? getLastNestedItem(items, prevItemId)
+      : prevItemId;
+  } else {
+    const parentId = getParentId(items, id);
+    if (!isRoot(parentId)) return parentId;
+  }
 };
 
 export const isOpen = (items: Items, id: string): boolean => !!items[id].isOpen;
@@ -113,3 +118,51 @@ export const mapChildrenIfOpen = <T>(
     return [];
   } else return [];
 };
+
+//helpers
+
+const getParentId = (items: Items, id: string): string => {
+  const i = Object.keys(items).find((key) => {
+    const item = items[key];
+    return item.children && item.children.indexOf(id) >= 0;
+  });
+
+  return i!;
+};
+
+const getContext = (items: Items, id: string): string[] => {
+  const parentId = getParentId(items, id);
+  return items[parentId].children || [];
+};
+
+const getParent = (items: Items, id: string): Item | undefined => {
+  const parentId = getParentId(items, id);
+  if (parentId) return items[parentId];
+};
+
+const getFollowingItem = (items: Items, id: string): string | undefined => {
+  const context = getContext(items, id);
+  const index = context.indexOf(id);
+  if (index < context.length - 1) {
+    return context[index + 1];
+  }
+};
+
+const isLast = (items: Items, id: string): boolean => {
+  const context = getContext(items, id);
+  return context.indexOf(id) === context.length - 1;
+};
+
+const getLastNestedItem = (items: Items, id: string): string => {
+  if (isOpen(items, id) && getChildren(items, id).length > 0) {
+    const children = getChildren(items, id);
+    return getLastNestedItem(items, children[children.length - 1]);
+  }
+  return id;
+};
+
+const getChildren = (items: Items, id: string): string[] =>
+  items[id].children || [];
+
+const roots = new Set(["HOME", "SEARCH"]);
+const isRoot = (id: string) => roots.has(id);
